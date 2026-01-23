0f97c7b5cea8a436d9bd4ff4d316d58147cef591
Romain Lechêne <romain_lechene@ens.univ-artois.fr>
2026-01-23 15:27:37 +0100

    Backbone of catch stick minigame

--- states/GameLoop.lua
+++ states/GameLoop.lua
@@ -18,7 +18,7 @@ function GameLoop:enter(params)
     -- Load all minigames identifiers
     self.availableMinigames = {}
     local minigameList = { 'taupe', 'minigame2', 'minigame3', 'minigame4', 'minigame5', 'popup', 'stocks-timing', 'taiko',
-        'time_matcher' }
+        'time_matcher', 'catch-stick', 'catch-stick' }
     for _, name in ipairs(minigameList) do
         local success, mg = pcall(require, 'minigames.' .. name .. '.init')
         if success then

e6527e4d90920f25a61e0dd1334d1c8454f2b4aa
theomortreux-a11y <theomortreux@gmail.com>
2026-01-23 18:42:15 +0100

    modif

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GAME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-flatpak run --filesystem="$GAME_DIR" org.love2d.love2d "$GAME_DIR"
+flatpak run --filesystem="$GAME_DIR" --socket=pulseaudio --device=all org.love2d.love2d "$GAME_DIR"
